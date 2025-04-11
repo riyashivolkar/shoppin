c68471aee32e33a783f30742432ab21ecd35ae50
Riya <64467020+riyashivolkar@users.noreply.github.com>
2025-04-11 15:36:01 +0530

    aa

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -16,6 +16,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+console.log("API KEY:", process.env.REACT_APP_FIREBASE_API_KEY);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
